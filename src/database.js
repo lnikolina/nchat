07d01fbfd56b3905ac9efeb8ae424730e1545f8e
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-08-28 17:44:09 +0200

    first commit

---
+++ src/database.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app"; // pullam odredene podatke - pullam app
+import "firebase/database"; // real time database
+
+const config = {
+    // API KLJUCEVI koje smo preuzeli s firebase-a
+
+        apiKey: "AIzaSyCOzbv8Aq-n_L3RUz644S_CURK_QK-McAw",
+        authDomain: "nchat-vue.firebaseapp.com",
+        projectId: "nchat-vue",
+        storageBucket: "nchat-vue.appspot.com",
+        messagingSenderId: "730699864216",
+        appId: "1:730699864216:web:7c21d4770ccce5832a18ed"
+    
+      
+}
+
+conts database = firebase.initializeApp (config);
+export default database; // exportam defaultne podatke u moju app.vue komponentu
